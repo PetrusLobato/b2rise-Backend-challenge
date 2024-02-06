9cbf29077b567c1ad7e0fe26f1c92d7d09a9f3d4
Petrus Lobato <admpetrusmlb@gmail.com>
2024-02-06 03:31:29 -0300

    add documentation swagger

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import productsRoute from "./routes/products_routes";
 import purchaseOrdersRoute from "./routes/orders_routes";
 import purchaseOrdersItemsRoute from "./routes/orders_items_routes";
 import { globalError } from "./errors/error";
+import swaggerUi from "swagger-ui-express";
+import swaggerrDocs from "./swagger.json";
+
 
 const app = express();
 app.use(express.json());
@@ -14,6 +17,8 @@ app.use("/users", usersRoute);
 app.use("/products", productsRoute);
 app.use("/orders", purchaseOrdersRoute);
 app.use("/orders/items", purchaseOrdersItemsRoute);
+
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerrDocs))
 
 app.use(globalError);
 
